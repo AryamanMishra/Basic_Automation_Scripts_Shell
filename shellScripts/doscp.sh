3dd80dcc5721493bc2177a1fd5131196f6080e5b
modest_aryaman09 <61847192+AryamanMishra@users.noreply.github.com>
2022-10-10 19:20:06 +0530

    first commit

---
+++ shellScripts/doscp.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#scp -r mapreduce aryamanm09@ip:F:\BigData
+
+echo "Enter File or Folder name: "
+read f_name
+
+echo "Enter User name for connecting machine"
+read user_name
+
+echo "Enter IP of connecting machine"
+read ip
+
+echo "Enter Destination Path"
+read dest_path
+
+if [ -d "$f_name" ]; then
+	scp -r $f_name $user_name@$ip:$dest_path
+elif [ -f "$f_name" ]; then
+	scp $f_name $user_name@ip:$dest_path
+else
+	echo "File/Folder does not exists"
+fi
+
+
