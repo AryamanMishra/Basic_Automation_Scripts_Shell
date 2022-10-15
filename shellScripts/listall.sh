79a40284d7687df69a35991b1be7134b3523b1ad
AryamanMishra <aryaman.m09@gmail.com>
2022-10-15 15:11:56 +0530

    added listall

--- shellScripts/listall.sh
+++ shellScripts/listall.sh
@@ -3,11 +3,47 @@
 echo "This command assumes you have set shellScripts folder in your PATH as stated in readme"
 
 
-SUB="/basic_automation_scripts_shell/shellScripts/"
+SUB="/shellScripts/"
 
+len=14
 if [[ "$PATH" == *"$SUB"* ]]; 
 then
-	echo $PATH
+	echo "To run them just type the command as it is, from anywhere in the file system (for example "doscp.sh") "
+	echo ""
+	echo "All shell commands in shellScripts folder :- "
+	colon=1
+	end=0
+	len=${#PATH}
+	for (( i=0;i<$len;i++ ));
+	do
+		if [[ "${PATH:$i:1}" = ':' ]];
+		then
+			colon=$i
+			colon=$((colon + 1))
+		fi
+		if [[ "${PATH:$i:14}" == "$SUB" ]]; 
+		then
+			end=$i
+			end=$((end + 14))
+			break
+		fi
+	done
+		
+	colon=$((colon + 1))
+	echo ""	
+	finalpath=`cut -c $colon-$end <<< "$PATH"`
+
+	cd $finalpath
+	
+	declare -a list=(`ls`)
+	for (( i=0;i<${#list};i++ ));
+	do
+		if [[ ${list[$i]} == *.sh ]];
+		then
+			echo ${list[$i]}
+		fi
+	done
+	
 else
 	echo "Nothing to display as path not set"
 fi	
